1eab154a2ce2f78b3b6bc14ecf8dc96e0953a637
yexudong <ye850570138@sina.com>
2016-02-04 14:16:45 +0800

    test

--- app/src/main/java/com/jike/androidrecyclerview/MainActivity.java
+++ app/src/main/java/com/jike/androidrecyclerview/MainActivity.java
@@ -2,7 +2,9 @@ package com.jike.androidrecyclerview;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.GridLayoutManager;
 import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.OrientationHelper;
 import android.support.v7.widget.RecyclerView;
 
 public class MainActivity extends AppCompatActivity {
@@ -18,9 +20,13 @@ public class MainActivity extends AppCompatActivity {
         /**
          * 设置线性布局管理器
          */
-        recyclerView.setLayoutManager(new LinearLayoutManager(this));
+        recyclerView.setLayoutManager(new GridLayoutManager(this, 3));
 
         recyclerView.setAdapter(new MyViewHolderAdapter(this));
+
+        recyclerView.addItemDecoration(new DividerItemDecoration
+                (this, DividerItemDecoration.VERTICAL_LIST));
+
 
     }
 
